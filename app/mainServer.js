7489e3d154d742fdcaa54f4c10e2b4a24f81fadd
Yusheng Ding <davidthinkleding@Gmail.com>
2016-06-26 23:48:01 -0700

    use express

---
+++ app/mainServer.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+var setPort = 3000; //server listen port 
+//var id;
+
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || setPort, function () {
+    var port = server.address().port;
+    console.log("Smartcar API system now running on port", port);
+  });
+
+//app.listen(port);
+
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+
+/*-----------------------------------------------------*/
+/*-----------------------------------------------------*/
+//default say hello
+app.get('/', function (req, res) {
+  res.send('Welcome to smartcar API system.');
+});
+
+//get Vehicle Info
+//return vin, color, doorCount, driveTrain
+app.get('/vehicles/:id', function(req, res){
+  console.log('['+req.method+']'+' Vehicle Info'+' :id = '+ req.params.id);
+  //var newVehicle = vehicleInfo(req.params.id);
+  res.json(111);
+});
+
+//get Security Info
+//return frontLeft, frontRight lock Info
+app.get('/vehicles/:id/doors', function(req, res){
+  console.log('['+req.method+']'+' Security Info'+' :id = '+ req.params.id);
+
+});
+
+//get Fuel Range
+//return fuel percentage
+app.get('/vehicles/:id/fuel', function(req, res){
+  console.log('['+req.method+']'+' Fuel Range'+' :id = '+ req.params.id);
+
+})
+
+//get Battery Range
+//return battery percentage
+app.get('/vehicles/:id/battery', function(req, res){
+  console.log('['+req.method+']'+' Vehicle Info'+' :id = '+ req.params.id);
+
+})
+
+//Post Engin Operation in json
+//return status: success|error
+app.post('/vehicles/:id/engin', function(req, res){
+  console.log('['+req.method+']'+' Engin Operation'+' :id = '+ req.params.id);
+});
+
+
