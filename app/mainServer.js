8d36df83421aa9826ddf6d990feeb59efba61e15
Yusheng Ding <davidthinkleding@Gmail.com>
2016-06-27 00:09:42 -0700

    add error handler

--- app/mainServer.js
+++ app/mainServer.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var setPort = 3000; //server listen port 
-//var id;
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || setPort, function () {
@@ -23,13 +24,13 @@ function handleError(res, reason, message, code) {
 /*-----------------------------------------------------*/
 /*-----------------------------------------------------*/
 //default say hello
-app.get('/', function (req, res) {
+app.get('/', function (req, res, handleError) {
   res.send('Welcome to smartcar API system.');
 });
 
 //get Vehicle Info
 //return vin, color, doorCount, driveTrain
-app.get('/vehicles/:id', function(req, res){
+app.get('/vehicles/:id', function(req, res, handleError) {
   console.log('['+req.method+']'+' Vehicle Info'+' :id = '+ req.params.id);
   //var newVehicle = vehicleInfo(req.params.id);
   res.json(111);
@@ -37,28 +38,28 @@ app.get('/vehicles/:id', function(req, res){
 
 //get Security Info
 //return frontLeft, frontRight lock Info
-app.get('/vehicles/:id/doors', function(req, res){
+app.get('/vehicles/:id/doors', function(req, res, handleError){
   console.log('['+req.method+']'+' Security Info'+' :id = '+ req.params.id);
 
 });
 
 //get Fuel Range
 //return fuel percentage
-app.get('/vehicles/:id/fuel', function(req, res){
+app.get('/vehicles/:id/fuel', function(req, res, handleError){
   console.log('['+req.method+']'+' Fuel Range'+' :id = '+ req.params.id);
 
 })
 
 //get Battery Range
 //return battery percentage
-app.get('/vehicles/:id/battery', function(req, res){
+app.get('/vehicles/:id/battery', function(req, res, handleError){
   console.log('['+req.method+']'+' Vehicle Info'+' :id = '+ req.params.id);
 
 })
 
 //Post Engin Operation in json
 //return status: success|error
-app.post('/vehicles/:id/engin', function(req, res){
+app.post('/vehicles/:id/engin', function(req, res, handleError){
   console.log('['+req.method+']'+' Engin Operation'+' :id = '+ req.params.id);
 });
 
